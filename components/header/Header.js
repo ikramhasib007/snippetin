6fb20a8a020b552cac5323d9d0d7f9fedf26e710
ikramhasib007 <ikramhasib007@gmail.com>
2018-12-30 23:18:10 +0600

    add nav menu

--- components/header/Header.js
+++ components/header/Header.js
@@ -1,6 +1,24 @@
+import Link from 'next/link';
+
 const Header = () => (
   <header>
-    <p>Snippet<span>In</span></p>
+    <Link href="/">
+      <a>Snippet<span>In</span></a>
+    </Link>
+    <nav>
+      <ul>
+        <li>
+          <Link href="/">
+            <a>Home</a>
+          </Link>
+        </li>
+        <li>
+          <Link href="/about">
+            <a>About</a>
+          </Link>
+        </li>
+      </ul>
+    </nav>
     <button>Login</button>
   </header>
 )
